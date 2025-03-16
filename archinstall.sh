ca6c7e8f28b839291bf71a26257321d89ea89777
Mr Falied <149243666+FaliedDedd@users.noreply.github.com>
2025-03-16 12:01:05 +0300

    Create archinstall.sh

---
+++ archinstall.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DISK="/dev/nvme0n1"
+BOOT_PART="${DISK}p1"
+ROOT_PART="${DISK}p2"
+USERNAME="falied"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Этот скрипт необходимо запускать с правами root."
+   exit 1
+fi
+
+sgdisk --zap-all $DISK
+sgdisk -n 1:0:+2G -t 1:ef00 -c 1:"EFI Boot" $DISK
+sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux Root" $DISK
+
+mkfs.fat -F32 $BOOT_PART
+mkfs.ext4 $ROOT_PART
+
+mount $ROOT_PART /mnt
+mkdir -p /mnt/boot
+mount $BOOT_PART /mnt/boot
+
+pacstrap /mnt base linux linux-firmware sudo grub efibootmgr gnome gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash <<EOF
+ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
+hwclock --systohc
+systemctl enable gdm
+systemctl enable NetworkManager
+echo "ru_RU.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
+useradd -m -G wheel -s /bin/bash $USERNAME
+echo "$USERNAME:password" | chpasswd
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+echo "Установка завершена. Перезагрузите систему."
