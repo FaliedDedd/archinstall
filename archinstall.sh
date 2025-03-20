88df9bc4135c9975512150233538016bca939602
Mr Falied <149243666+FaliedDedd@users.noreply.github.com>
2025-03-20 17:03:03 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "Check disk partition"
-echo "Using disk: /dev/sda"
 
-DISK="/dev/sda"
+echo "Check disk partition"
+echo "Using disk: /dev/nvme0n1"
+
+DISK="/dev/nvme0n1"
 BOOT_PART="${DISK}p1"
 ROOT_PART="${DISK}p2"
 USERNAME="falied"
@@ -13,27 +14,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 
-if [ ! -b "$DISK" ]; then
-    echo "Disk $DISK does not exist."
-    exit 1
-fi
-
-
 sgdisk --zap-all $DISK
 if [ $? -ne 0 ]; then
     echo "Failed to zap the disk. Please check if the disk is in use."
     exit 1
 fi
 
-sgdisk -n 1:0:+2G -t 1:ef00 -c 1:"EFI Boot" $DISK
-if [ $? -ne 0 ]; then
-    echo "Failed to create EFI partition."
-    exit 1
-fi
-
+sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI Boot" $DISK
 sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux Root" $DISK
 if [ $? -ne 0 ]; then
-    echo "Failed to create root partition."
+    echo "Failed to create partitions."
     exit 1
 fi
 
@@ -41,12 +31,13 @@ fi
 mkfs.fat -F32 $BOOT_PART
 mkfs.ext4 $ROOT_PART
 
+
 mount $ROOT_PART /mnt
 mkdir -p /mnt/boot
 mount $BOOT_PART /mnt/boot
 
 
-pacstrap /mnt base linux linux-firmware sudo grub efibootmgr gnome gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family nvidia nvidia-utils xf86-video-amdgpu
+pacstrap /mnt base linux linux-firmware sudo grub efibootmgr gnome gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family firefox nvidia
 
 
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -64,26 +55,6 @@ useradd -m -G wheel -s /bin/bash $USERNAME
 passwd
 echo "$USERNAME:password" | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-# Настройка Xorg для использования NVIDIA по умолчанию
-mkdir -p /etc/X11/xorg.conf.d
-cat <<EOL > /etc/X11/xorg.conf.d/10-nvidia.conf
-Section "Device"
-    Identifier "NVIDIA Card"
-    Driver "nvidia"
-    Option "AllowEmptyInitialConfiguration" "true"
-EndSection
-EOL
-
-cat <<EOL > /etc/X11/xorg.conf.d/20-amd.conf
-Section "Device"
-    Identifier "AMD Card"
-    Driver "amdgpu"
-EndSection
-EOL
-
-# Установка nvidia-prime
-pacman -S --noconfirm nvidia-prime
 
 # Установка GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
