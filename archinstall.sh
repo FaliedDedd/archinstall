02e43f126167de8fe08f97d03087271058fdd539
Mr Falied <149243666+FaliedDedd@users.noreply.github.com>
2025-03-20 18:33:23 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,70 +1,109 @@
 #!/bin/bash
+echo "Check disk partition"
+echo "Available disks:"
+lsblk -d -o NAME,SIZE,TYPE,MOUNTPOINT | grep disk
+echo "Please write your disk name (e.g., nvme0n1 or sda):"
+read disk
+echo "What is your name?"
+read user
 
+if [ ! -b "/dev/$disk" ]; then
+    echo "Disk /dev/$disk does not exist. Please check the name and try again."
+    exit 1
+fi
 
-
-
-DISK="/dev/nvme0n1"
-
+lsblk
+echo "Using disk: /dev/$disk"
+DISK="/dev/$disk"
 BOOT_PART="${DISK}p1"
-
 ROOT_PART="${DISK}p2"
-
-
-USERNAME="falied"
-
-
+USERNAME="$user"
 
 if [[ $EUID -ne 0 ]]; then
+   echo "Please start this script with root permissions."
+   exit 1
+fi
 
-   echo "Please start this script with root perm."
+sgdisk --zap-all $DISK
+if [ $? -ne 0 ]; then
+    echo "Failed to zap the disk. Please check if the disk is in use."
+    exit 1
+fi
 
-@@ -21,24 +21,24 @@
+sgdisk -n 1:0:+2G -t 1:ef00 -c 1:"EFI Boot" $DISK
+sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux Root" $DISK
+if [ $? -ne 0 ]; then
+    echo "Failed to create partitions."
+    exit 1
+fi
+
+mkfs.fat -F32 $BOOT_PART
+mkfs.ext4 $ROOT_PART
+
+mount $ROOT_PART /mnt
+if [ $? -ne 0 ]; then
+    echo "Failed to mount root partition."
+    exit 1
+fi
 
 mkdir -p /mnt/boot
-
 mount $BOOT_PART /mnt/boot
+if [ $? -ne 0 ]; then
+    echo "Failed to mount boot partition."
+    exit 1
+fi
 
-
-
-
-pacstrap /mnt base linux linux-firmware sudo grub efibootmgr gnome gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family
-
-
+pacstrap /mnt base linux linux-firmware sudo grub efibootmgr cinnamon gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family nvidia nvidia-utils xf86-video-amdgpu nodejs npm
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-
-
 arch-chroot /mnt /bin/bash <<EOF
-
 ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
-
 hwclock --systohc
-
 systemctl enable gdm
-
 systemctl enable NetworkManager
-
 echo "ru_RU.UTF-8 UTF-8" > /etc/locale.gen
-
 locale-gen
-
 echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
 
-useradd -m -G wheel -s /bin/bash $USERNAME
+read -sp "Enter root password: " root_password
+echo "root:\$root_password" | chpasswd
 
-passwd
+if id "$USERNAME" &>/dev/null; then
+    echo "User '$USERNAME' already exists."
+else
+    useradd -m -G wheel -s /bin/bash $USERNAME
+    read -sp "Enter password for user $USERNAME: " user_password
+    echo "$USERNAME:\$user_password" | chpasswd
+    echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+fi
 
-echo "$USERNAME:password" | chpasswd
+read -p "Do you want to add NVIDIA configuration? (y/n): " add_nvidia
+if [[ $add_nvidia == "y" || $add_nvidia == "Y" ]]; then
+    mkdir -p /etc/X11/xorg.conf.d
+    cat <<EOL > /etc/X11/xorg.conf.d/10-nvidia.conf
+Section "Device"
+    Identifier "NVIDIA Card"
+    Driver "nvidia"
+    Option "AllowEmptyInitialConfiguration" "true"
+EndSection
+EOL
+    echo "NVIDIA configuration added."
+fi
 
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+read -p "Do you want to add AMD configuration? (y/n): " add_amd
+if [[ $add_amd == "y" || $add_amd == "Y" ]]; then
+    mkdir -p /etc/X11/xorg.conf.d
+    cat <<EOL > /etc/X11/xorg.conf.d/20-amd.conf
+Section "Device"
+    Identifier "AMD Card"
+    Driver "amdgpu"
+EndSection
+EOL
+    echo "AMD configuration added."
+fi
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-grub-mkconfig -o /boot/grub/grub.cfg
+npm install -g n
+n latest
 
 EOF
-
-
-
-echo "Install complete. Please settings sudoers and create passwd -m username"
