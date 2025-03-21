d6f3875c9fe976fe44e3a8fecda20af86744fd5c
Mr Falied <149243666+FaliedDedd@users.noreply.github.com>
2025-03-21 13:57:06 +0300

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,75 +1,70 @@
 #!/bin/bash
-echo "Check disk partition"
-echo "Available disks:"
-lsblk -d -o NAME,SIZE,TYPE,MOUNTPOINT | grep disk
-echo "Please write your disk name (e.g., nvme0n1 or sda):"
-read disk
 
-if [ ! -b "/dev/$disk" ]; then
-    echo "Disk /dev/$disk does not exist. Please check the name and try again."
-    exit 1
-fi
 
-lsblk
-echo "Using disk: /dev/$disk"
-DISK="/dev/$disk"
+
+
+DISK="/dev/nvme0n1"
+
 BOOT_PART="${DISK}p1"
+
 ROOT_PART="${DISK}p2"
 
+
+USERNAME="falied"
+
+
+
 if [[ $EUID -ne 0 ]]; then
-   echo "Please start this script with root permissions."
-   exit 1
-fi
 
-sgdisk --zap-all $DISK
-if [ $? -ne 0 ]; then
-    echo "Failed to zap the disk. Please check if the disk is in use."
-    exit 1
-fi
+   echo "Please start this script with root perm."
 
-sgdisk -n 1:0:+2G -t 1:ef00 -c 1:"EFI Boot" $DISK
-sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux Root" $DISK
-if [ $? -ne 0 ]; then
-    echo "Failed to create partitions."
-    exit 1
-fi
-
-mkfs.fat -F32 $BOOT_PART
-mkfs.ext4 $ROOT_PART
-
-mount $ROOT_PART /mnt
-if [ $? -ne 0 ]; then
-    echo "Failed to mount root partition."
-    exit 1
-fi
+@@ -21,24 +21,24 @@
 
 mkdir -p /mnt/boot
-mount $BOOT_PART /mnt/boot
-if [ $? -ne 0 ]; then
-    echo "Failed to mount boot partition."
-    exit 1
-fi
 
-pacstrap /mnt base linux linux-firmware sudo grub efibootmgr
+mount $BOOT_PART /mnt/boot
+
+
+
+
+pacstrap /mnt base linux linux-firmware sudo grub efibootmgr gnome gdm base-devel nano vim networkmanager git xorg ttf-ubuntu-font-family
+
+
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
+
+
 arch-chroot /mnt /bin/bash <<EOF
+
 ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
+
 hwclock --systohc
+
 systemctl enable gdm
+
 systemctl enable NetworkManager
+
 echo "ru_RU.UTF-8 UTF-8" > /etc/locale.gen
+
 locale-gen
+
 echo "LANG=ru_RU.UTF-8" > /etc/locale.conf
 
-# Set root password
-echo "root:root_password" | chpasswd
-
-# Create a user
-USERNAME="user"
 useradd -m -G wheel -s /bin/bash $USERNAME
-echo "$USERNAME:user_password" | chpasswd
+
+passwd
+
+echo "$USERNAME:password" | chpasswd
+
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
 EOF
+
+
+
+echo "Install complete. Please settings sudoers and create passwd -m username"
